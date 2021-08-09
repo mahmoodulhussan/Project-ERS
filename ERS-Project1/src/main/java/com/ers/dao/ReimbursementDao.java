3c0b9117a6701d9a80e51181055cb140cb463b6a
mahmoodulhussan <86261716+mahmoodulhussan@users.noreply.github.com>
2021-08-09 10:55:03 -0500

    08/09/2021 10:32 AM Central

---
+++ ERS-Project1/src/main/java/com/ers/dao/ReimbursementDao.java
@@ -0,0 +1,43 @@
+package com.ers.dao;
+
+import java.util.List;
+
+import com.ers.enums.RType;
+import com.ers.enums.Status;
+import com.ers.models.Reimbursement;
+import com.ers.models.ReimbursementStatus;
+import com.ers.models.ReimbursementType;
+import com.ers.models.User;
+import com.ers.models.UserRoles;
+
+public interface ReimbursementDao {
+	
+	List<Reimbursement> getAllReimbursments();
+	
+	Reimbursement getReimbursementById(int id);
+	
+	void createReimbursement(Reimbursement r);
+
+	void updateReimbursement(Reimbursement r);
+	
+	void deleteReimbursement(Reimbursement r);
+	
+	void deleteReimbursementById(int id);
+	
+	void updateReimbursementByArgs(int id, ReimbursementType type, double amount, String submitteddate, String resolveddate, String description, ReimbursementStatus status, User employeeId, User Manager);
+	
+//	void updateReimbursementByArgs(ReimbursementStatus r, UserRoles id, Reimbursement date);
+
+	
+	List<Reimbursement> getAllPendingReimbursments();
+	
+	List<Reimbursement> getAllAcceptedReimbursments();
+	
+	List<Reimbursement> getAllDeniedReimbursments();
+	
+	List<Reimbursement> getAllPendingReimbursmentsForUser(User u);
+	
+	List<Reimbursement> getAllAcceptedReimbursmentsForUser(User u);
+	
+	List<Reimbursement> getAllDeniedReimbursmentsForUser(User u);
+}
